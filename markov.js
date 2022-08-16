a58c5166458745f1525f2b45f5d30df93f2897ed
Alex <alexjooho@gmail.com>
2022-08-16 16:39:10 -0700

    finished step 1

--- markov.js
+++ markov.js
@@ -1,3 +1,4 @@
+"use strict";
 /** Textual markov chain generator. */
 
 
@@ -27,7 +28,17 @@ class MarkovMachine {
    * */
 
   getChains() {
-    // TODO: implement this!
+    let chains = {};
+
+    for(let i =0; i < this.words.length; i++) {
+      if(i === this.words.length - 1) {
+        chains[this.words[i]] ? chains[this.words[i]].push(null) : chains[this.words[i]] = [null]
+      }
+      else {
+        chains[this.words[i]] ? chains[this.words[i]].push(this.words[i+1]) : chains[this.words[i]] = [this.words[i+1]];
+      }
+    }
+    return chains;
   }
 
 
@@ -35,10 +46,48 @@ class MarkovMachine {
    *  until it hits a null choice. */
 
   getText() {
-    // TODO: implement this!
 
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+
+    let text = this.words[0];
+    let lastWord = this.words[0];
+
+    while(lastWord !== null) {
+      let chainLength = this.chains[lastWord].length;
+      let randomIndex = Math.floor(Math.random()*chainLength);
+      lastWord = this.chains[lastWord][randomIndex];
+      if(lastWord !== null) {
+        text += ` ${lastWord}`;
+      }
+    }
+
+    return text;
+
   }
 }
+
+const catInHatMachine = new MarkovMachine("The cat is in the hat. The cat is the cat. The hat is a cat.");
+
+console.log(catInHatMachine.getText());
+
+/*
+const fsp = require("fs/promises")
+undefined
+> async function readFile() {
+... let contents = await fsp.readFile("gettysburg.txt", "utf8");
+... return contents;
+... }
+undefined
+> let test2;
+undefined
+> async function writeFile() {
+... let content = await readFile();
+... test2 = new MarkovMachine(content);
+... }
+undefined
+> writeFile();
+ */
+
+module.exports = {MarkovMachine};
